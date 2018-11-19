294850032949362346750cf1bd24666e9e3d7137
Carlos McNulty <cmcnul3@uic.edu>
2018-11-18 22:48:26 -0600

    improved error reporting

--- js/event-handlers.js
+++ js/event-handlers.js
@@ -16,6 +16,9 @@ var $usernameFeedback = $('#username-feedback');
 // Password input
 var $password = $('#password');
 
+// Key input
+var $key = $('#key');
+
 // Button objects
 var $submitButton = $('#submit-button');
 var $showButton   = $('#checkbox');
@@ -23,7 +26,7 @@ var $showButton   = $('#checkbox');
 // Filters
 var lowerCaseLetters  	= /[a-z]/g;
 var upperCaseLetters 	= /[A-Z]/g;
-var specialCharacters	= /[~`!#$%\^&.*()+=\-\[\]\\';,/{}|\\":<>\?]/g
+var specialCharacters	= /[~`!#$%\^&.*()+=\-\[\]\\';,/{}|\\":<>\?_]/g
 var numbers 			= /[0-9]/g;
 
 /* Updates password icons if valid
@@ -33,7 +36,7 @@ var numbers 			= /[0-9]/g;
  * indicating that the password isn't
  * ready.
  */
-function updatePasswordIcons(password){
+function validatePassword(password){
 	var valid = true;
 	// Validate lower case letters
 	if(password.match(lowerCaseLetters)){
@@ -92,49 +95,7 @@ function updatePasswordIcons(password){
  * using the show button.
  */
 function togglePasswordViz() {
-	var passwordElem = document.getElementById('password');
-	if(passwordElem.type === 'password'){
-		passwordElem.type = 'text';
-	}
-	else{
-		passwordElem.type = 'password';
-	}
-}
-
-/* Handle change of focus for username field.
- * Provides username feedback if username isn't
- * at least 8 characters long.
- */
-$username.on('change', function(){
-	var username = $(this).val();
-	// Check username length and set
-	// appropriate feedback message
-	if(username.length < 8){
-		$usernameFeedback.text('Username must be 8 characters or more');
-	}
-	else{
-		$usernameFeedback.text('');
-	}
-});
-
-/* Handle keyup event for password input.
- */ 
-$password.on('keyup', function() {
-	var password = $(this).val();
-	// Change color to indicate completion
-	if(updatePasswordIcons(password)){
-		$submitButton.css('background-color', '#33ff33');
-	}
-	else{
-		$submitButton.css('background-color', '#ccffcc');
-	}
-
-});
-
-/* Handle click event for password
- * to toggle password visibility.
- */
-$showButton.on('click', function() {
+	// Get the type attribute from password
 	var type = $password.attr('type');
 	// Toggle password type from plain-text to asterisks
 	if(type == 'password'){
@@ -143,9 +104,178 @@ $showButton.on('click', function() {
 	else{
 		$password.attr('type', 'password');
 	}
+}
+
+/* Set error message in element.
+ */
+function setErrorMessage(el, message){
+	$(el).data('message', message);
+}
+
+/* Adds an error message belonging to the
+ * named element after the element. 
+ */
+function addErrorMessage(el){
+	var $el = $(el);
+	// Collect existing errors
+	var $errors = $el.siblings('.error');
+	// Test for no existing errors
+	if(!$errors.length){
+		// Create span to hold error message and add after el
+		$errors = $el.after('<div class="error"></div>');
+	}
+	// Add error message
+	$errors.text($el.data('message'));
+}
+
+/* Clears an existing error message.
+ */
+function clearErrorMessage(el){
+	var $el = $(el);
+	// Collect existing errors
+	var $errors = $el.siblings('.error');
+	// Test for existing error
+	if($errors.length > 0){
+		// Clear error message
+		$errors.text('');
+	}
+}
+
+/* Object with functions to validate
+ * fields.
+ */
+var validateFields = {
+	password: function(el){
+		// Validate that password meets requirements
+		// and update requirement icons
+		var valid = validatePassword(el.value);
+		if(!valid){
+			setErrorMessage(el, 'Password missing requirements');
+		}
+		return valid;
+		
+	},
+	username: function(el){
+		// Check that username has at least 8 characters
+		var valid = (el.value.length >= 8);
+		if(!valid){
+			setErrorMessage(el, "Username must be at least 8 characters long");
+		}
+		return valid;
+	},
+	key: function(el){
+		// Check that invitation key has correct format
+		var valid = /[\w]{4}-[\w]{4}-[\w]{4}/.test(el.value);
+		if(!valid){
+			setErrorMessage(el, "Key has invalid form");
+		}
+		return valid;
+	}
+}
+
+/* Checks that the given element is a valid
+ * field.
+ */
+function validateField(el){
+	// Find the name attribute to determine the kind of field
+	// which as of now can be either username, password, or key
+	var fieldType = $(el).data('name') || el.getAttribute('name');
+	// Check if type is a function of the validateFields object
+	if(typeof validateFields[fieldType] == 'function'){
+		// Check if the element is valid
+		return (el.value) && validateFields[fieldType](el);
+	}
+	else{
+		// Return true if not a 
+		// function of validateFields
+		return true;
+	}
+}
+
+/* Validate each field in the form. Returns
+ * true if every field is completed and 
+ * meets the requirements.
+ */
+function validateForm(el){
+	var form = document.getElementById(el);
+	// Get form elements
+	var elems = form.elements;
+	var validForm = true;
+	// Validate each element in form
+	for (i = 0; i < elems.length; i++) {
+		// Validate element
+		var isValid = validateField(elems[i]);
+		// And values to record at least one error
+		validForm = validForm && isValid;
+		if(!isValid){
+			// Add error message to page
+			addErrorMessage(elems[i]);
+		}
+		else{
+			// Remove error message from page
+			clearErrorMessage(elems[i]);
+		}
+	}
+	return validForm;
+}
+
+/* Handle change of for username field.
+ * Provides username feedback if username isn't
+ * at least 8 characters long.
+ */
+$username.on('input', function(){
+	if(validateForm('login')){
+		$submitButton.css('background-color', '#33ff33');
+	}
+	else{
+		$submitButton.css('background-color', '#ccffcc');
+	}
 });
 
+/* Handle change of password field.
+ */ 
+$password.on('input', function() {
+	if(validateForm('login')){
+		$submitButton.css('background-color', '#33ff33');
+	}
+	else{
+		$submitButton.css('background-color', '#ccffcc');
+	}
+});
 
+/* Handle change of key field.
+ */
+$key.on('input', function(){
+	if(validateForm('login')){
+		$submitButton.css('background-color', '#33ff33');
+	}
+	else{
+		$submitButton.css('background-color', '#ccffcc');
+	}
+});
 
+/* Handle click event for password
+ * to toggle password visibility.
+ */
+$showButton.on('click', function() {
+	// Toggle the password visibility
+	togglePasswordViz();
+});
+
+/* Handle submit event for sign-up form
+ * by validating input before submitting.
+ */
+$('#login').on('submit', function(e){
+	// Check that the form is valid
+	if(validateForm('login')){
+		$submitButton.css('background-color', '#33ff33');
+	}
+	else{
+		// Prevent submission
+		e.preventDefault();
+		e.stopPropagation();
+		$submitButton.css('background-color', '#ccffcc');
+	}
+});
 
 /* ---------------------- End Username/Password Check ---------------------- */
